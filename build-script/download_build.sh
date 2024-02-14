011dd070f420791937b9f2c25fbd39b504e367bd
Darmur <dario.murgia@alumni-live.uniroma2.it>
2024-02-14 22:44:45 +0100

    add build script, patch and source file for custom waveshare-dsi driver

---
+++ build-script/download_build.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+CPU=4
+KERNEL_VERSION="6.1.69"
+
+case $KERNEL_VERSION in
+    "6.1.70")
+      KERNEL_COMMIT="fc9319fda550a86dc6c23c12adda54a0f8163f22"
+      PATCH="custom-6.1.x.patch"
+      ;;
+    "6.1.69")
+      KERNEL_COMMIT="ec8e8136d773de83e313aaf983e664079cce2815"
+      PATCH="custom-6.1.x.patch"
+      ;;
+    "6.1.64")
+      KERNEL_COMMIT="01145f0eb166cbc68dd2fe63740fac04d682133e"
+      PATCH="custom-6.1.x.patch"
+      ;;
+    "6.1.58")
+      KERNEL_COMMIT="7b859959a6642aff44acdfd957d6d66f6756021e"
+      PATCH="custom-6.1.x.patch"
+      ;;
+    "6.1.57")
+      KERNEL_COMMIT="12833d1bee03c4ac58dc4addf411944a189f1dfd"
+      PATCH="custom-6.1.x.patch"
+      ;;
+esac
+
+echo "!!!  Build modules for kernel ${KERNEL_VERSION}  !!!"
+echo "!!!  Download kernel hash info  !!!"
+wget -N https://raw.githubusercontent.com/raspberrypi/rpi-firmware/${KERNEL_COMMIT}/git_hash
+GIT_HASH="$(cat git_hash)"
+rm git_hash
+
+echo "!!!  Download kernel source  !!!"
+wget https://github.com/raspberrypi/linux/archive/${GIT_HASH}.tar.gz
+
+echo "!!!  Extract kernel source  !!!"
+rm -rf linux-${KERNEL_VERSION}+/
+tar xvzf ${GIT_HASH}.tar.gz
+rm ${GIT_HASH}.tar.gz
+mv linux-${GIT_HASH}/ linux-${KERNEL_VERSION}+/
+
+echo "!!!  Create git repo and apply patch  !!!"
+cd linux-${KERNEL_VERSION}+/
+git init
+git add --all
+git commit -m "extracted files"
+cp ../../patch/${PATCH} ${PATCH}
+git apply ${PATCH}
+git status
+cd ..
+
+echo "!!!  Copy source files for other variants  !!!"
+rm -rf linux-${KERNEL_VERSION}-v7+/
+rm -rf linux-${KERNEL_VERSION}-v7l+/
+cp -r linux-${KERNEL_VERSION}+/ linux-${KERNEL_VERSION}-v7+/
+cp -r linux-${KERNEL_VERSION}+/ linux-${KERNEL_VERSION}-v7l+/
+cp -r linux-${KERNEL_VERSION}+/ linux-${KERNEL_VERSION}-v8+/
+
+echo "!!!  Build RPi0 kernel and modules  !!!"
+cd linux-${KERNEL_VERSION}+/
+KERNEL=kernel
+make -j${CPU} ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcmrpi_defconfig
+make -j${CPU} ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
+cd ..
+echo "!!!  RPi0 build done  !!!"
+echo "-------------------------"
+
+echo "!!!  Build RPi2 kernel and modules  !!!"
+cd linux-${KERNEL_VERSION}-v7+/
+KERNEL=kernel7
+make -j${CPU} ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2709_defconfig
+make -j${CPU} ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
+cd ..
+echo "!!!  RPi2 build done  !!!"
+echo "-------------------------"
+
+echo "!!!  Build RPi3/4 32-bit kernel and modules  !!!"
+cd linux-${KERNEL_VERSION}-v7l+/
+KERNEL=kernel7l
+make -j${CPU} ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2711_defconfig
+make -j${CPU} ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
+cd ..
+echo "!!!  RPi3/4 build 32-bit done  !!!"
+echo "-------------------------"
+
+echo "!!!  Build RPi3/4/5 64-bit kernel and modules  !!!"
+cd linux-${KERNEL_VERSION}-v8+/
+KERNEL=kernel8
+make -j${CPU} ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
+make -j${CPU} ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
+cd ..
+echo "!!!  RPi3/4/5 64-bit build done  !!!"
+echo "-------------------------"
+
+echo "!!!  Creating archive  !!!"
+rm -rf modules-rpi-${KERNEL_VERSION}-custom/
+mkdir -p modules-rpi-${KERNEL_VERSION}-custom/lib/modules/${KERNEL_VERSION}+/kernel/drivers/gpu/drm/panel/
+mkdir -p modules-rpi-${KERNEL_VERSION}-custom/lib/modules/${KERNEL_VERSION}-v7+/kernel/drivers/gpu/drm/panel/
+mkdir -p modules-rpi-${KERNEL_VERSION}-custom/lib/modules/${KERNEL_VERSION}-v7l+/kernel/drivers/gpu/drm/panel/
+mkdir -p modules-rpi-${KERNEL_VERSION}-custom/lib/modules/${KERNEL_VERSION}-v8+/kernel/drivers/gpu/drm/panel/
+cp linux-${KERNEL_VERSION}+/drivers/gpu/drm/panel/panel-waveshare-dsi.ko* modules-rpi-${KERNEL_VERSION}-custom//lib/modules/${KERNEL_VERSION}+/kernel/drivers/gpu/drm/panel/
+cp linux-${KERNEL_VERSION}-v7+/drivers/gpu/drm/panel/panel-waveshare-dsi.ko* modules-rpi-${KERNEL_VERSION}-custom/lib/modules/${KERNEL_VERSION}-v7+/kernel/drivers/gpu/drm/panel/
+cp linux-${KERNEL_VERSION}-v7l+/drivers/gpu/drm/panel/panel-waveshare-dsi.ko* modules-rpi-${KERNEL_VERSION}-custom/lib/modules/${KERNEL_VERSION}-v7l+/kernel/drivers/gpu/drm/panel/
+cp linux-${KERNEL_VERSION}-v8+/drivers/gpu/drm/panel/panel-waveshare-dsi.ko* modules-rpi-${KERNEL_VERSION}-custom/lib/modules/${KERNEL_VERSION}-v8+/kernel/drivers/gpu/drm/panel/
+tar -czvf modules-rpi-${KERNEL_VERSION}-custom.tar.gz modules-rpi-${KERNEL_VERSION}-custom/ --owner=0 --group=0
+md5sum modules-rpi-${KERNEL_VERSION}-custom.tar.gz > modules-rpi-${KERNEL_VERSION}-custom.md5sum.txt
+sha1sum modules-rpi-${KERNEL_VERSION}-custom.tar.gz > modules-rpi-${KERNEL_VERSION}-custom.sha1sum.txt
+rm -rf modules-rpi-${KERNEL_VERSION}-custom/
+mkdir -p ../output
+mv modules-rpi-${KERNEL_VERSION}-custom* ../output/
+
+echo "!!!  Done  !!!"
