605e36cab7b8117b6786202e6dc6dfd62e421008
Darmur <dario.murgia@alumni-live.uniroma2.it>
2024-11-25 11:40:44 +0100

    use a different patch for kernel 6.6.56 and higher

--- build-script/re-build.sh
+++ build-script/re-build.sh
@@ -6,11 +6,11 @@ KERNEL_VERSION="6.6.62"
 case $KERNEL_VERSION in
     "6.6.62")
       KERNEL_COMMIT="9a9bda382acec723c901e5ae7c7f415d9afbf635"
-      PATCH="custom-6.6.x.patch"
+      PATCH="custom-6.6.y.patch"
       ;;
     "6.6.56")
       KERNEL_COMMIT="a5efb544aeb14338b481c3bdc27f709e8ee3cf8c"
-      PATCH="custom-6.6.x.patch"
+      PATCH="custom-6.6.y.patch"
       ;;
     "6.6.51")
       KERNEL_COMMIT="d5a7dbe77b71974b9abb133a4b5210a8070c9284"
