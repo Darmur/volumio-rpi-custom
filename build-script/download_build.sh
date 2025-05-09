7dc794485158974e462cb51ae0cc63a3e12b74b7
Darmur <dario.murgia@alumni-live.uniroma2.it>
2025-05-09 17:16:53 +0200

    add hash, patch and package for kernel 6.12.27

--- build-script/download_build.sh
+++ build-script/download_build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 CPU=4
-KERNEL_VERSION="6.6.62"
+KERNEL_VERSION="6.12.27"
 
 case $KERNEL_VERSION in
+    "6.12.27")
+      KERNEL_COMMIT="f54e67fef6e726725d3a8f56d232194497bd247c"
+      PATCH="custom-6.12.x.patch"
+      ;;
     "6.6.62")
       KERNEL_COMMIT="9a9bda382acec723c901e5ae7c7f415d9afbf635"
       PATCH="custom-6.6.y.patch"
